3357da82f9e12cf9b7519c5d9e2d60b773b3300a
Giovanni Bordiga <gio.61192@gmail.com>
2023-07-21 10:42:35 -0400

    Refactor energy using strain fn

--- morphing_lattices/energy.py
+++ morphing_lattices/energy.py
@@ -1,17 +1,17 @@
 import jax.numpy as jnp
+from morphing_lattices.kinematics import build_strain_fn
 from morphing_lattices.structure import ControlParams
 
 
 def build_strain_energy(connectivity):
 
+    strain_fn = build_strain_fn(connectivity)
+
     def strain_energy(displacement: jnp.ndarray, control_params: ControlParams):
+        strain = strain_fn(control_params.reference_points, displacement)
         reference_points = control_params.reference_points
-        current_points = reference_points + displacement
         reference_vectors = reference_points[connectivity[:, 1]] - reference_points[connectivity[:, 0]]
-        current_vectors = current_points[connectivity[:, 1]] - current_points[connectivity[:, 0]]
         reference_lengths = jnp.linalg.norm(reference_vectors, axis=-1)
-        current_lengths = jnp.linalg.norm(current_vectors, axis=-1)
-        strain = (current_lengths - reference_lengths) / reference_lengths
 
         young = control_params.young
         area = control_params.area
@@ -20,7 +20,7 @@ def build_strain_energy(connectivity):
         # NOTE: If we want to include the change of cross-sectional area, we can add it here with a dedicated alpha_transverse.
         # From experiments: alpha_transverse ~ alpha_longitudinal.
         return 0.5 * jnp.sum(
-            young*area * reference_lengths * ((1+strain)/(1+thermal_strain) - 1)**2
+            young * area * reference_lengths * ((1+strain)/(1+thermal_strain) - 1)**2
         )
 
     return strain_energy
