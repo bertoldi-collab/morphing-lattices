22abe0452c184620297d681c6c05a3a888433e76
Giovanni Bordiga <gio.61192@gmail.com>
2023-11-16 14:54:05 -0500

    Reformat code and add docstrings

--- morphing_lattices/structure.py
+++ morphing_lattices/structure.py
@@ -4,18 +4,39 @@ import jax.numpy as jnp
 
 
 class ControlParams(NamedTuple):
+    """Parameters that define the lattice structure and can be optimized.
+
+    Attrs:
+        reference_points (jnp.ndarray): Array of shape (n_points, 2) representing the reference nodal positions of the lattice.
+        young (jnp.ndarray): Array of shape (n_bonds,) representing the Young's modulus of each struts.
+        area (jnp.ndarray): Array of shape (n_bonds,) representing the cross-sectional area of each struts.
+        thermal_strain (jnp.ndarray): Array of shape (n_bonds,) representing the thermal strain of each struts.
+        damping (jnp.ndarray): Array of shape (n_points, 2) representing the damping of each point.
+        masses (jnp.ndarray): Array of shape (n_points, 2) representing the mass of each point.
+        loading_params (Dict): Loading parameters to be passed to loading_fn. Default: {}.
+        constraint_params (Dict): Constraint parameters to be passed to constraint_fn. Default: {}.
+    """
+
     reference_points: jnp.ndarray
-    young: jnp.ndarray  # Stiffness in the reference configuration. This will be a function of temperature.
-    area: jnp.ndarray  # Cross-sectional areas in the reference configuration.
-    thermal_strain: jnp.ndarray  # Thermal strain. This will be a function of temperature.
-    damping: jnp.ndarray  # Damping coefficient.
-    masses: jnp.ndarray  # Masses of the points.
-    loading_params: Dict = dict()  # Loading parameters to be passed to loading functions. Default: {}.
-    constraint_params: Dict = dict()  # Constraint parameters to be passed to constraint_DOFs_fn. Default: {}.
+    young: jnp.ndarray
+    area: jnp.ndarray
+    thermal_strain: jnp.ndarray
+    damping: jnp.ndarray
+    masses: jnp.ndarray
+    loading_params: Dict = dict()
+    constraint_params: Dict = dict()
 
 
 @dataclass
 class Lattice:
+    """Lattice structure.
+
+    Attrs:
+        connectivity (jnp.ndarray): Array of shape (n_bonds, 2) representing the connectivity of the lattice.
+        control_params (ControlParams): Parameters that define the lattice structure and can be optimized.
+        solution (jnp.ndarray): Array of shape (n_timepoints, 2, n_points, 2) representing the nodal positions of the lattice. Default: None.
+    """
+
     connectivity: jnp.ndarray
     control_params: ControlParams
     solution: Optional[jnp.ndarray] = None
@@ -29,6 +50,8 @@ class Lattice:
         points = self.control_params.reference_points
         x_ext = points[:, 0].max() - points[:, 0].min()
         y_ext = points[:, 1].max() - points[:, 1].min()
-        xlim = (points[:, 0].min() - extra_x*x_ext, points[:, 0].max() + extra_x*x_ext)
-        ylim = (points[:, 1].min() - extra_y*y_ext, points[:, 1].max() + extra_y*y_ext)
+        xlim = (points[:, 0].min() - extra_x*x_ext,
+                points[:, 0].max() + extra_x*x_ext)
+        ylim = (points[:, 1].min() - extra_y*y_ext,
+                points[:, 1].max() + extra_y*y_ext)
         return xlim, ylim
