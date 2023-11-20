d09588b8870652944cf7b65c822b130afe0f026b
Giovanni Bordiga <gio.61192@gmail.com>
2023-11-20 18:29:23 -0500

    Refactor plot_lattice function to include x_label and y_label parameters

--- morphing_lattices/plotting.py
+++ morphing_lattices/plotting.py
@@ -9,22 +9,26 @@ from morphing_lattices.kinematics import rotation_matrix
 import jax.numpy as jnp
 
 
-def plot_lattice(lattice: Lattice, displacement=None, xlim=None, ylim=None, title="Lattice", figsize=(5, 5), annotate=False, bond_values=None, LTNI_bond_indices = None, HTNI_bond_indices = None, node_size=None, legend_label=None, fontsize=14, cmap="coolwarm", axis=True):
-    points = lattice.control_params.reference_points if displacement is None else displacement + lattice.control_params.reference_points
+def plot_lattice(lattice: Lattice, displacement=None, xlim=None, ylim=None, title="Lattice", x_label=None, y_label=None, figsize=(5, 5), annotate=False, bond_values=None, LTNI_bond_indices=None, HTNI_bond_indices=None, node_size=None, legend_label=None, fontsize=14, cmap="coolwarm", axis=True):
+    points = lattice.control_params.reference_points if displacement is None else displacement + \
+        lattice.control_params.reference_points
     connectivity = lattice.connectivity
 
     # Plot the lattice
     fig, ax = plt.subplots(constrained_layout=True, figsize=figsize)
     ax.set_aspect("equal")
-    ax.set_xlabel("$x$", fontsize=fontsize)
-    ax.set_ylabel("$y$", fontsize=fontsize)
+    if x_label is not None:
+        ax.set_xlabel(x_label, fontsize=fontsize)
+    if y_label is not None:
+        ax.set_ylabel(y_label, fontsize=fontsize)
     ax.set_title(title, fontsize=fontsize)
     ax.tick_params(labelsize=0.8*fontsize)
     if not axis:
         ax.axis("off")
 
     # Plot nodes
-    ax.scatter(points[:, 0], points[:, 1], color="black", zorder=10, s=node_size)
+    ax.scatter(points[:, 0], points[:, 1],
+               color="black", zorder=10, s=node_size)
     # Plot the bonds
     if bond_values is not None:
         # Color the bonds
@@ -36,7 +40,8 @@ def plot_lattice(lattice: Lattice, displacement=None, xlim=None, ylim=None, titl
         sm.set_array(bond_values)
         cb = fig.colorbar(sm, ax=ax, pad=0.04, aspect=25)
         cb.ax.tick_params(labelsize=0.8*fontsize)
-        cb.ax.set_ylabel(legend_label if legend_label is not None else "", fontsize=fontsize)
+        cb.ax.set_ylabel(
+            legend_label if legend_label is not None else "", fontsize=fontsize)
     else:
         colors = None
 
@@ -46,19 +51,24 @@ def plot_lattice(lattice: Lattice, displacement=None, xlim=None, ylim=None, titl
         if LTNI_bond_indices is not None and HTNI_bond_indices is not None:
             if i in LTNI_bond_indices:
                 if cntr_LTNI == 0:
-                    ax.plot(*points[pair].T, lw=2, color='#069AF3' if colors is None else colors[i], label='LTNI')
+                    ax.plot(
+                        *points[pair].T, lw=2, color='#069AF3' if colors is None else colors[i], label='LTNI')
                     cntr_LTNI = cntr_LTNI+1
                 else:
-                    ax.plot(*points[pair].T, lw=2, color='#069AF3' if colors is None else colors[i])
+                    ax.plot(
+                        *points[pair].T, lw=2, color='#069AF3' if colors is None else colors[i])
             elif i in HTNI_bond_indices:
                 if cntr_HTNI == 0:
-                    ax.plot(*points[pair].T, lw=2, color="#F97306" if colors is None else colors[i], label='HTNI')
+                    ax.plot(
+                        *points[pair].T, lw=2, color="#F97306" if colors is None else colors[i], label='HTNI')
                     cntr_HTNI = cntr_HTNI+1
                 else:
-                    ax.plot(*points[pair].T, lw=2, color="#F97306" if colors is None else colors[i])
-        
+                    ax.plot(
+                        *points[pair].T, lw=2, color="#F97306" if colors is None else colors[i])
+
         else:
-            ax.plot(*points[pair].T, lw=2, color="#2980b9" if colors is None else colors[i])
+            ax.plot(*points[pair].T, lw=2,
+                    color="#2980b9" if colors is None else colors[i])
     if LTNI_bond_indices is not None and HTNI_bond_indices is not None:
         ax.legend()
     if annotate:
@@ -69,7 +79,8 @@ def plot_lattice(lattice: Lattice, displacement=None, xlim=None, ylim=None, titl
 
     # xylim
     _xlim, _ylim = lattice.get_xy_limits()
-    ax.set(xlim=_xlim if xlim is None else xlim, ylim=_ylim if ylim is None else ylim)
+    ax.set(xlim=_xlim if xlim is None else xlim,
+           ylim=_ylim if ylim is None else ylim)
 
     return fig, ax
 
@@ -89,27 +100,33 @@ def generate_animation(lattice: Lattice, solution: jnp.ndarray, lattice_number=N
     ax.tick_params(labelsize=tick_size)
     if not axis:
         ax.axis("off")
-    
+
     if lattice_number is not None:
-        ax.add_patch(matplotlib.patches.Rectangle((xlim[0], ylim[0]+0.9*(ylim[1] - ylim[0])), 0.0*(xlim[1] - xlim[0]), 0.0*(ylim[1] - ylim[0]), color='#F97306', label='HTNI'))
-        ax.add_patch(matplotlib.patches.Rectangle((xlim[0], ylim[0]+0.8*(ylim[1] - ylim[0])), 0.0*(xlim[1] - xlim[0]), 0.0*(ylim[1] - ylim[0]), color='#069AF3', label='LTNI'))
+        ax.add_patch(matplotlib.patches.Rectangle((xlim[0], ylim[0]+0.9*(ylim[1] - ylim[0])), 0.0*(
+            xlim[1] - xlim[0]), 0.0*(ylim[1] - ylim[0]), color='#F97306', label='HTNI'))
+        ax.add_patch(matplotlib.patches.Rectangle((xlim[0], ylim[0]+0.8*(ylim[1] - ylim[0])), 0.0*(
+            xlim[1] - xlim[0]), 0.0*(ylim[1] - ylim[0]), color='#069AF3', label='LTNI'))
         ax.legend()
 
     # First frame
     connectivity = lattice.connectivity
-    if rotated_points and lattice_number==10:
-        points = jnp.dot(rotation_matrix(-jnp.pi/2), (lattice.control_params.reference_points + solution[0, 0]).T).T
+    if rotated_points and lattice_number == 10:
+        points = jnp.dot(rotation_matrix(-jnp.pi/2),
+                         (lattice.control_params.reference_points + solution[0, 0]).T).T
         points0 = points.copy()
         points = points.at[:, 1].set(points[:, 1]-points0[9, 1])
         # Nodes
-        scatter_plot = ax.scatter(points[:, 0], points[:, 1], color="black", zorder=10, s=node_size)
+        scatter_plot = ax.scatter(
+            points[:, 0], points[:, 1], color="black", zorder=10, s=node_size)
     else:
         points = lattice.control_params.reference_points + solution[0, 0]
         # Nodes
-        scatter_plot = ax.scatter(points[:, 0], points[:, 1], color="black", zorder=10, s=node_size)
-    
+        scatter_plot = ax.scatter(
+            points[:, 0], points[:, 1], color="black", zorder=10, s=node_size)
+
     # Bonds
-    collection_bonds = LineCollection(points[connectivity], color="black", linewidth=2)
+    collection_bonds = LineCollection(
+        points[connectivity], color="black", linewidth=2)
     ax.add_collection(collection_bonds)
     if bond_color is not None:
         # Color the bonds with a single color
@@ -131,12 +148,14 @@ def generate_animation(lattice: Lattice, solution: jnp.ndarray, lattice_number=N
         cb.ax.set_ylabel(legend_label, fontsize=fontsize)
     # xylim
     _xlim, _ylim = lattice.get_xy_limits()
-    ax.set(xlim=_xlim if xlim is None else xlim, ylim=_ylim if ylim is None else ylim)
+    ax.set(xlim=_xlim if xlim is None else xlim,
+           ylim=_ylim if ylim is None else ylim)
 
     def animate(i):
         # Update nodes
-        if rotated_points and lattice_number==10:
-            points = jnp.dot(rotation_matrix(-jnp.pi/2), (lattice.control_params.reference_points + solution[i, 0]).T).T
+        if rotated_points and lattice_number == 10:
+            points = jnp.dot(rotation_matrix(-jnp.pi/2),
+                             (lattice.control_params.reference_points + solution[i, 0]).T).T
             points = points.at[:, 1].set(points[:, 1] - points0[9, 1])
             scatter_plot.set_offsets(points)
         else:
@@ -152,7 +171,9 @@ def generate_animation(lattice: Lattice, solution: jnp.ndarray, lattice_number=N
         return scatter_plot, collection_bonds
 
     frames = range(solution.shape[0]) if frame_range is None else frame_range
-    anim = animation.FuncAnimation(fig, animate, frames=frames, blit=True)  # type: ignore
+    anim = animation.FuncAnimation(
+        fig, animate, frames=frames, blit=True)  # type: ignore
     out_path = Path(f"{out_filename}.mp4")
-    out_path.parent.mkdir(parents=True, exist_ok=True)  # Make sure parents directories exist
+    # Make sure parents directories exist
+    out_path.parent.mkdir(parents=True, exist_ok=True)
     anim.save(str(out_path), writer="ffmpeg", fps=fps, dpi=dpi)
